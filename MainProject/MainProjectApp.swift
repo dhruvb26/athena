dea3d0bc1b0e75318e7c381c100063c056752562
Dhruv Bansal <dk.bansal0026@gmail.com>
2025-03-25 14:25:31 -0700

    Initial Commit

---
+++ MainProject/MainProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MainProjectApp.swift
+//  MainProject
+//
+//  Created by Dhruv Bansal on 3/25/25.
+//
+
+import SwiftUI
+
+@main
+struct MainProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
